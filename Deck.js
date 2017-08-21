d3b20ab375ebb000cc259316b0d1890f0449cf58
Seth Watson <seth.watson1@gmail.com>
2017-08-21 00:09:11 -0600

    added const, Deck  value,Deck  suit

--- Deck.js
+++ Deck.js
@@ -1,5 +1,9 @@
+const NUMBER_OF_CARDS = 52;
+const NUMBERS_IN_SUIT = 13;
+
 class Deck {
     constructor() {
-
+        let value = (Math.floor(Math.random() * NUMBER_OF_CARDS) + 1) % NUMBERS_IN_SUIT;
+        let suit = Math.floor(Math.random() * 4) + 1;
     }
 };
