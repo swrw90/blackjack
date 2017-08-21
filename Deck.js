1829cdcdb4cc0f36896eef878dd17c43032053fa
Seth Watson <seth.watson1@gmail.com>
2017-08-21 00:48:42 -0600

    added genCard func to Deck

--- Deck.js
+++ Deck.js
@@ -3,7 +3,10 @@ const NUMBERS_IN_SUIT = 13;
 
 class Deck {
     constructor() {
+    }
+    static generateCard() {
         let value = (Math.floor(Math.random() * NUMBER_OF_CARDS) + 1) % NUMBERS_IN_SUIT;
         let suit = Math.floor(Math.random() * 4) + 1;
+        return new Card(suit, value);
     }
 };
