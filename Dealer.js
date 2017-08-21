23f833fb125ea017d79a6d944f5cd45633afe90b
Seth Watson <seth.watson1@gmail.com>
2017-08-21 01:10:25 -0600

    added to Dealer

--- Dealer.js
+++ Dealer.js
@@ -1,5 +1,26 @@
 class Dealer {
-    constructor() {
-        
+    constructor(hand) {
+        this.hand = hand;
+        this.isDone = false;
+        this.isBusted = false;
     }
-}
+
+    get totalOfHand() {
+        let handTotal = 0;
+        for (let i = 0; i < this.hand.length; i++) {
+            handTotal += this.hand[i].value;
+        };
+        return handTotal;
+    }
+
+    hit() {
+        this.hand.push(Deck.hit());
+        if (this.totalOfHand > 21) {
+            this.isBusted = true;
+        }
+    };
+
+    stay() {
+        this.isDone = true;
+    }
+};
