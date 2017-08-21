080cf8c64b56f4f18c989ecc7171b848eaa26a54
Seth Watson <seth.watson1@gmail.com>
2017-08-21 00:40:24 -0600

    added properties to Card, added switch to suit func

--- Card.js
+++ Card.js
@@ -1,13 +1,20 @@
+// King is 0 because of modulo 13 of King is 0
 const KING = 0;
 const JACK = 11;
 const QUEEN = 12;
 
 class Card {
-    constructor() {
-
+    constructor(suitValue, number) {
+        this._suitValue = suitValue;
+        this._number = number;
     }
     get suit() {
-
+        switch (this._suitValue) {
+            case 1: return "Spades";
+            case 2: return "Clubs";
+            case 3: return "Hearts";
+            case 4: return "Diamonds";
+        }
     }
 
     get value() {
